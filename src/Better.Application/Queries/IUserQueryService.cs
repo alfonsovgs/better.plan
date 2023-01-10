a2c5d56362865b0e170254c59f58d5cf1e8b183b
alfonsovgs <vc.alfonso@htomail.com>
2023-01-10 12:36:11 -0600

    feat: updated userQueryService

---
+++ src/Better.Application/Queries/IUserQueryService.cs
@@ -0,0 +1,10 @@
+﻿using Better.Application.Common.Models;
+using Better.Application.DTO;
+
+namespace Better.Application.Queries;
+
+public interface IUserQueryService
+{
+    Task<UserDto> GetUser(int id);
+    Task<PaginatedList<GoalDto>> GetGoalsByUserId(int id, int pageNumber, int pageSize);
+}
