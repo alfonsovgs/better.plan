0050802056a057f26fa8e13f201c72261fd12558
alfonsovgs <vc.alfonso@htomail.com>
2023-01-10 11:52:06 -0600

    feat: added user dto

---
+++ src/Better.Application/Users/DTO/UserDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Better.Application.Common.Mappings;
+using Better.Core.Entities;
+
+namespace Better.Application.Customers.DTO;
+
+public class UserDto : IMapFrom<User>
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string Advisor { get; set; }
+    public DateTime CreatedAt { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<User, UserDto>()
+            .ForMember(x => x.Username, opt => opt.MapFrom(src => $"{src.Firstname} {src.Surname}"))
+            .ForMember(x => x.Advisor, opt => opt.MapFrom(src => src.Advisor == null ? string.Empty : $"{src.Advisor.Firstname} {src.Advisor.Surname}"))
+            .ForMember(x => x.CreatedAt, opt => opt.MapFrom(src => src.Created));
+    }
+}
