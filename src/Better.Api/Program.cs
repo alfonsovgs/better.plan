434ac5413e11dc4950839695c9481a74d687478c
alfonsovgs <vc.alfonso@htomail.com>
2023-01-10 11:55:11 -0600

    feat: configure services

--- src/Better.Api/Program.cs
+++ src/Better.Api/Program.cs
@@ -1,15 +1,18 @@
+using Better.Application;
 using Better.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHealthChecks();
 builder.Services.AddSwaggerGen();
 
 builder.Services
-    .AddInfrastructure();
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -23,5 +26,11 @@ if (app.Environment.IsDevelopment())
 app.MapHealthChecks("/healthz");
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.MapControllers();
 
 app.Run();
