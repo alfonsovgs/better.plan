982214b7b7dffae0a30695136830dc8c9b062859
alfonsovgs <vc.alfonso@htomail.com>
2023-01-13 10:08:42 -0600

    feat: implemented GetSummaryByUserId

---
+++ src/Better.Application/DTO/SummaryDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Better.Application.Common.Mappings;
+using Better.Core.Entities;
+
+namespace Better.Application.DTO;
+
+public class SummaryDto : IMapFrom<Balance>
+{
+    public double Balance { get; set; }
+    public double CurrentContributions { get; set; }
+
+    public void Mapping(Profile profile)
+    {
+        profile.CreateMap<Balance, SummaryDto>()
+            .ForMember(x => x.Balance, opt => opt.MapFrom(src => src.CurrentAmount))
+            .ForMember(x => x.CurrentContributions, opt => opt.MapFrom(src => src.Total));
+    }
+}
