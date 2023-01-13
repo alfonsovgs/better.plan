6dd6cdf34bb74308fc235c80ac2b75076f0831c9
alfonsovgs <vc.alfonso@htomail.com>
2023-01-13 10:13:10 -0600

    refactor: code clean up

--- src/Better.Application/Services/CurrencyIndicatorLookup.cs
+++ src/Better.Application/Services/CurrencyIndicatorLookup.cs
@@ -1,12 +1,10 @@
 ﻿using Better.Core.Entities;
 using Better.Core.Repositories;
-using System.Collections.Concurrent;
 
 namespace Better.Application.Services;
 
 internal class CurrencyIndicatorLookup : ICurrencyIndicatorLookup
 {
-    private readonly ConcurrentDictionary<string, CurrencyIndicator> _currencyIndicatorLookup = new();
     private readonly ICurrencyIndicatorRepository _repository;
 
     public CurrencyIndicatorLookup(ICurrencyIndicatorRepository repository)
@@ -18,16 +16,5 @@ internal class CurrencyIndicatorLookup : ICurrencyIndicatorLookup
     {
         var currencyIndicator = await _repository.GetBy(sourceCurrency, destinationCurrency, date);
         return currencyIndicator ?? CurrencyIndicator.Default;
-        /*string key = $"src{sourceCurrency}:dst{destinationCurrency}:date{date}";
-
-        if (!_currencyIndicatorLookup.TryGetValue(key, out var value))
-        {
-            var currencyIndicator = await _repository.GetBy(sourceCurrency, destinationCurrency, date);
-
-            value = currencyIndicator ?? CurrencyIndicator.Default;
-            _currencyIndicatorLookup.TryAdd(key, value);
-        }
-
-        return value;*/
     }
 }
