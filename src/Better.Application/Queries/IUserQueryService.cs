c0038ceaf467cf75ce928a2cdcbba64c9ca13179
alfonsovgs <vc.alfonso@htomail.com>
2023-01-14 14:18:24 -0600

    feat: define method to get summary

--- src/Better.Application/Queries/IUserQueryService.cs
+++ src/Better.Application/Queries/IUserQueryService.cs
@@ -8,4 +8,5 @@ public interface IUserQueryService
     Task<UserDto> GetUser(int id);
     Task<PaginatedList<GoalDto>> GetGoalsByUserId(int id, int pageNumber, int pageSize);
     Task<SummaryDto> GetSummaryByUserId(int id);
+    Task<GoalDetailDto> GetGoalDetail(int id, int goalId);
 }
